7d75654687d36041d7ed8cb51f1568fb8696f48c
arturlbg <arturlbg31@gmail.com>
2024-09-22 15:01:52 -0300

    initial commmit

---
+++ test/clj_auth_audit/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-auth-audit.core-test
+  (:require [clojure.test :refer :all]
+            [clj-auth-audit.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
